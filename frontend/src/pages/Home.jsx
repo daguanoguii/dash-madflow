18f0aa765287480d04cd566347413af03dcf9e3f
daguanoguii <guidaguano@gmail.com>
2026-02-03 10:58:36 -0300

    Destruturação de código

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import LogoutIcon from "@mui/icons-material/Logout"
 import AssignmentIcon from '@mui/icons-material/Assignment'
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
 import { getDiaMesAtual } from '../hooks/home_page/DiaeMes'
+import { Sidebar } from "../components/layouts/SideBar"
 
 
 export const Home = () => {
@@ -20,34 +21,7 @@ export const Home = () => {
   return (
     <Box sx={{ display: "flex" }}>
       {/* SIDEBAR */}
-      <Box
-        sx={{
-          width: open ? 220 : 80,
-          height: "100vh",
-          backgroundColor: "#3e3283fc",
-          transition: "width 0.3s",
-          display: "flex",
-          flexDirection: "column",
-          p: 1
-        }}
-      >
-        {/* Botão menu */}
-        <IconButton onClick={() => setOpen(!open)} sx={{ color: "white", mb: 2 }}>
-          <MenuIcon />
-        </IconButton>
-
-        <SidebarItem icon={<HomeIcon />} label="Home" open={open} />
-        <SidebarItem icon={<EventIcon />} label="Consultas" open={open} />
-        <SidebarItem icon={<PeopleIcon />} label="Pacientes" open={open} />
-        <SidebarItem icon={<AssignmentIcon />} label="Protuários" open={open} />
-        <SidebarItem icon={<BarChartIcon />} label="Relatórios" open={open} />
-        <SidebarItem icon={<AttachMoneyIcon />} label="Financeiro" open={open} />
-        
-
-        <Box sx={{ flexGrow: 1 }} />
-            <SidebarItem icon={<SettingsIcon />} label="Configurações" open={open} />
-            <SidebarItem icon={<LogoutIcon />} label="Sair" open={open} />
-        </Box>
+      <Sidebar open={open} setOpen={setOpen}/>
 
       {/* CONTEÚDO */}
       <Box sx={{ flexGrow: 1, p: 3 }}>
